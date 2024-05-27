40af1b855e87414df7d8711921d0481190c3aee1
CornaSn <cornelia.schenk@hotmail.com>
2024-05-27 16:40:27 +0200

    Delet useEffekt by POST method

--- src/App.js
+++ src/App.js
@@ -3,39 +3,37 @@ import { useEffect, useState } from 'react';
 const baseUrl = 'http://localhost:4000';
 
 export default function App() {
-  const [guests, setGuests] = useState([]);
+  const [guestList, setGuestList] = useState([]);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [isAttending, setIsAttending] = useState(false);
 
-  useEffect(() => {
-    // POST new guest to the guest list
-    async function addGuestToList() {
-      // Variable for the POST request
-      const guestPost = {
-        firstName: firstName,
-        lastName: lastName,
-        attending: false,
-      };
-      const response = await fetch(`${baseUrl}/guests`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(guestPost),
-      });
-      const data = await response.json();
-      console.log(data);
-    }
+  // Add new guest to List with POST method
+  async function addGuestToList() {
+    // Variable for the POST request
+    const newGuestInfo = {
+      firstName: firstName,
+      lastName: lastName,
+      attending: false,
+    };
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newGuestInfo),
+    });
+    const data = await response.json();
+    console.log(data);
     addGuestToList().catch((error) => console.log(error));
-  }, [guests]);
+  }
 
+  // Show whole guest list with GET method
   useEffect(() => {
-    // GET all guests from the guest list
     async function showGuestList() {
       const response = await fetch(`${baseUrl}/guests`);
       const data = await response.json();
-      setGuests(data);
+      setGuestList(data);
     }
     showGuestList().catch((error) => console.log(error));
   }, []);
@@ -53,21 +51,19 @@ export default function App() {
           id="LastName"
           value={lastName}
           onChange={(event) => setLastName(event.currentTarget.value)}
-          onKeyDown={(event) => {
-            const newGuest = {
-              firstName: firstName,
-              lastName: lastName,
-            };
-            if (event.key === 'Enter') {
-              const newGuestsList = [...guests];
-              newGuestsList.push(newGuest);
-              setGuests(newGuestsList);
-            }
-          }}
+          // onKeyDown={(event) => {
+          //   const newGuest = {
+          //     firstName: firstName,
+          //     lastName: lastName,
+          //   };
+          //   if (event.key === 'Enter') {
+          //     addGuestToList();
+          //   }
+          // }}
         />
         <label htmlFor="LastName">Last name</label>
-      </form>
-      {/* <button
+
+        {/* <button
         onClick={(event) => {
           const newGuest = {
             keyId: guests[guests.length - 1].keyId + 1,
@@ -82,31 +78,33 @@ export default function App() {
         {' '}
         Add Guest
       </button> */}
-      <h1>Guest List</h1>
-      <div>
-        {guests.map((guest) => {
-          return (
-            <div key={`guest-${guest.id}`}>
-              <h2>
-                {guest.firstName} {guest.lastName}
-              </h2>
-              <div>Guests ID: {guest.id}</div>
-              <div>
-                <label>
-                  Attending: {guest.attending}
-                  <input
-                    type="checkbox"
-                    checked={isAttending}
-                    onChange={(event) =>
-                      setIsAttending(event.currentTarget.checked)
-                    }
-                  />
-                </label>
+        <h1>Guest List</h1>
+        <div>
+          {guestList.map((guest) => {
+            return (
+              <div key={`guest-${guest.id}`}>
+                <h4>
+                  {guest.firstName} {guest.lastName}
+                </h4>
+                <div>Guests ID: {guest.id}</div>
+                <div>
+                  <aria-label>
+                    {isAttending ? 'attending' : 'not attending'}
+                    <input
+                      type="checkbox"
+                      defaultChecked={isAttending}
+                      checked={isAttending}
+                      onChange={(event) =>
+                        setIsAttending(event.currentTarget.checked)
+                      }
+                    />
+                  </aria-label>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </form>
     </div>
   );
 }
